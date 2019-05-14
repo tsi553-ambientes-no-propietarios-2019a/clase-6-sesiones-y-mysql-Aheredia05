7ceedca7e11d9bbf3ab7a6ad7f8f7e5a0aa800d0
Alfonso Heredia <aheredia.estudio@gmail.com>
2019-05-14 14:22:14 -0500

    ejercicios

--- ejercicio3/index.php
+++ ejercicio3/index.php
@@ -2,7 +2,8 @@
 /*
 Basados en el ejemplo 2:
 a) Controlar que solo inicie sesión si se han escrito el usuario y contraseña en los campos. 
-b) Validar el usuario y contraseña contra una lista de usuarios y contraseñas almacenados en un array o una base de datos (tabla usuarios).
+b) Validar el usuario y contraseña contra una lista de usuarios y contraseñas almacenados en un array o 
+una base de datos (tabla usuarios).
 c) Crear un formulario que le permita registrar un usuario con los siguientes datos: 
      - nombre
      - apellidos
@@ -13,8 +14,36 @@ c) Crear un formulario que le permita registrar un usuario con los siguientes da
      - correo electrónico
      - fecha de nacimiento
      - ciudad de nacimiento (lista de ciudades)
-     - intereses: programación, redes, ensamblaje, SOA, BI, análisis de texto, big data, redes sociales   (checkbox)
+     - intereses: programación, redes, ensamblaje, SOA, BI, análisis de texto, big data, redes sociales
+        (checkbox)
      - una breve biografía
-d) El formulario anterior debe ser enviado a través de un botón submit mediante POST y debe guardar la información en la base de datos (tabla usuarios) y presentar una página con toda la información ingresada en el formulario, esta página debe incluir un link que permita volver a ingresar los datos y un link que permita cerrar la sesión y volver al index.
+d) El formulario anterior debe ser enviado a través de un botón submit mediante POST 
+y debe guardar la información en la base de datos (tabla usuarios) y presentar una página con 
+toda la información ingresada en el formulario, esta página debe incluir un link que permita volver 
+a ingresar los datos y un link que permita cerrar la sesión y volver al index.
 */
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1><center>Login</h1>
+	
+    <form method="POST" action="php/login.php">
+	<center><input type="text" name="username" placeholder="Usuario"> 
+  <br>
+  <br>
+		<input type="password" name="password" placeholder="Password">
+		<br>
+    <br>
+    <button>Ingresar</button>
+		
+	</center>
+	</form>
+
+	</center>
+</body>
+</html>
