c44f7d10419e36584d2e2acd9954ca7a4d418691
Alfonso Heredia <aheredia.estudio@gmail.com>
2019-05-14 13:54:43 -0500

    ejercicio2

---
+++ ejercicio2/login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-squiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+</head>
+<body>
+    <center>
+        <form action="logica/loguear.php" method="POST">
+        <H1>lOGIN</H1><BR>
+            <input type="text" name="usuario" placeholder="digite usuario">
+            <br><br>
+            <input type="password" name="clave" placeholder="digite clave">
+            <br><br>
+            <button type="submit">Entrar</button>
+
+        </form>
+    </center>
+</body>
+</html>
